50bbab3ff2eb5a897148ccd3a5b296d49a462dc2
JohnIsutsa <37325810+JohnIsutsa@users.noreply.github.com>
2024-01-23 12:25:03 +0300

    Update default.ts

--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,3 @@
 import getAPIBaseUrl from "@/utils/env.util";
 
-export const SOCKET_URL = `${getAPIBaseUrl()}/chat` || 'http://localhost:9900/chat';
+export const SOCKET_URL = `${getAPIBaseUrl()}/chat`;
